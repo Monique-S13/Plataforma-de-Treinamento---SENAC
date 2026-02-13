7bf28aa81bb7717425bf1f2aa9522ddaa6c8b8ac
Monique <moniquesilva13000@gmail.com>
2026-02-13 11:58:16 -0300

    Foi tirado codigo extra que não seria utilizado
    Removed commented-out code and unnecessary comments.

--- conteudos.js
+++ conteudos.js
@@ -1,20 +1,3 @@
-// function menuF() {
-//     barC = document.getElementByClassName("fa-bars");
-//     barI = document.getElementByClassName("fa-bars-staggered ");
-
-//     if (){
-//         barI.classList.toggle(ocultarB);
-//     } else{
-//         barC.classList.toggle(ocultarB);
-//     }
-    
-// }
-
-
-// eu quero uma função que quando eu clico na barra, o menu se expande, quando feixo, os textos fica none e só fica os icones centralizados no menu
-
-
-
 const btnM = document.getElementById("btn-menu");
 const menuAb = document.getElementById("menu");
 const elemTextN = document.querySelectorAll(".textMenu");
@@ -39,7 +22,3 @@ sectAbaOp.addEventListener('click', (event) => {
         sectAbaOp.classList.remove("ativo-aba-op");
     }
 });
-
-// document.getElementById("btn_op_p").addEventListener('click', () => {
-
-// });
