38a976cd74357a4d2a25af16244413cbd84cab99
Monique-S13 <moniquesilva13000@gmail.com>
2026-02-13 06:27:50 -0300

    Add de alguns eventos e etc
    Foi modificado cores, adicionado eventos de click no menu, e no botão do main, criação dos arquivos da tela onde ficará os vídeos

--- conteudos.js
+++ conteudos.js
@@ -12,13 +12,34 @@
 
 
 // eu quero uma função que quando eu clico na barra, o menu se expande, quando feixo, os textos fica none e só fica os icones centralizados no menu
-let btnM = document.getElementById("btn-menu");
-let elemTextN = document.getElementsByClassName("elem-text-menu");
 
-btnM.addEventListener('click', () =>{
-    
-})
 
-if (elem1 === 60px){
-    el
-}
+
+const btnM = document.getElementById("btn-menu");
+const menuAb = document.getElementById("menu");
+const elemTextN = document.querySelectorAll(".textMenu");
+
+btnM.addEventListener('click', () => {
+    menuAb.classList.toggle("menuAberto");
+
+    elemTextN.forEach(texto => {
+        texto.classList.toggle("elem-text-menu");
+    });
+});
+
+const btnAbaOp = document.getElementById("btn-treinamentos");
+const sectAbaOp = document.getElementById("sect-aba-op");
+
+btnAbaOp.addEventListener('click', () => {
+    sectAbaOp.classList.add("ativo-aba-op");
+});
+
+sectAbaOp.addEventListener('click', (event) => {
+    if (event.target === sectAbaOp){
+        sectAbaOp.classList.remove("ativo-aba-op");
+    }
+});
+
+// document.getElementById("btn_op_p").addEventListener('click', () => {
+
+// });
